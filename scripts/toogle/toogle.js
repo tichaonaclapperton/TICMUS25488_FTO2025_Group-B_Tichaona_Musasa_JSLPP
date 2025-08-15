7ba628f14f1ed12705a642e6477b3423494a3360
unknown <clappertontee@gmail.com>
2025-08-15 11:32:44 +0200

    I have fixed the changing of colors when you toggle the button

--- scripts/toogle/toogle.js
+++ scripts/toogle/toogle.js
@@ -1,19 +1,26 @@
 export function sideBarToogle(){
-    const toggle = document.getElementById("themeToggle");
+const toggle = document.getElementById("themeToggle");
 const body = document.body;
 const sidebar = document.getElementById("side-bar-div");
 const hideSidebarBtn = document.getElementById("hideSidebar");
 const darkLight = document.getElementById("logoDark");
 const lightLogo = document.getElementById("logo");
+const hideSwitch = document.getElementById("hideSwitch");
 
 toggle.addEventListener("change", () => {
 	body.classList.toggle("dark-mode");
 	if (body.classList.contains("dark-mode")) {
 		darkLight.style.display = "block";
 		lightLogo.style.display = "none";
+		hideSwitch.style.backgroundColor = 'rgba(99, 95, 199, 1)';
+		hideSidebarBtn.style.backgroundColor = 'rgba(99, 95, 199, 1)';
+		hideSidebarBtn.style.color= 'white';
 	} else {
 		darkLight.style.display = "none";
 		lightLogo.style.display = "block";
+		hideSwitch.style.backgroundColor = 'rgba(244, 247, 253, 1)';
+		hideSidebarBtn.style.backgroundColor = 'rgba(244, 247, 253, 1)';
+		hideSidebarBtn.style.color= 'rgba(99, 95, 199, 1)';
 	}
 	renderTasks(tasks);
 });
